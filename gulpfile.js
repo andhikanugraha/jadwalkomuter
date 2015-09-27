a490eecc949a4bf15bc7817bcb889e35866bdefd
Andhika Nugraha <andhika.nugraha@outlook.com>
2015-09-27 21:26:23 +0700

    Refactored gulpfile, build, fetch

--- gulpfile.js
+++ gulpfile.js
@@ -9,27 +9,47 @@ var serve = require('gulp-serve');
 var source = require('vinyl-source-stream');
 var uglify = require('gulp-uglify');
 
-var fetchTimetables = require('./fetchTimetables');
-var buildHTML = require('./buildHTML');
+var fetchTimetables = require('./lib/fetchTimetables');
+var buildHTML = require('./lib/buildHTML');
 
-gulp.task('sass', () => {
+gulp.task('default', ['build']);
+
+gulp.task('fetch', ['fetch:json']);
+gulp.task('copy', ['copy:fonts']);
+gulp.task('build', ['copy', 'build:css', 'build:html', 'build:js']);
+gulp.task('clean', () => {
+  return del(['dist/**', 'dist', '!dist/.git']);
+});
+
+gulp.task('serve', serve('dist'));
+
+gulp.task('fetch:json', () => {
+  return fetchTimetables();
+});
+gulp.task('clean:json', () => {
+  return del(['dist/*/index.json', 'dist/index.json']);
+});
+
+gulp.task('build:html', () => {
+  return buildHTML();
+});
+gulp.task('clean:html', () => {
+  return del(['dist/*/index.html']);
+});
+
+gulp.task('build:css', () => {
   return gulp.src('src/scss/*.scss')
     .pipe(sass({
-      outputStyle: 'nexted',
+      outputStyle: 'compressed',
       includePaths: ['node_modules']
     }))
     .pipe(gulp.dest('dist/css'));
 });
-
-gulp.task('fetch', () => {
-  return fetchTimetables();
+gulp.task('clean:css', () => {
+  return del(['dist/css']);
 });
 
-gulp.task('html', () => {
-  return buildHTML();
-});
-
-gulp.task('js', ['timetable', 'index'].map(fn => {
+gulp.task('build:js', ['timetable', 'index'].map(fn => {
   gulp.task(`js:${fn}`, () =>
       browserify({
         entries: `src/js/${fn}.js`,
@@ -43,27 +63,11 @@ gulp.task('js', ['timetable', 'index'].map(fn => {
       .pipe(gulp.dest('dist/js/')));
   return `js:${fn}`;
 }));
-
-gulp.task('clean:css', () => {
-  return del(['dist/css']);
-});
-
 gulp.task('clean:js', () => {
   return del(['dist/js']);
 });
 
-gulp.task('clean:html', () => {
-  return del(['dist/*/index.html']);
+gulp.task('copy:fonts', () => {
+  return gulp.src('src/fonts/*')
+    .pipe(gulp.dest('dist/fonts'));
 });
-
-gulp.task('clean:json', () => {
-  return del(['dist/*/index.json', 'dist/index.json']);
-});
-
-gulp.task('clean', () => {
-  return del(['dist/**', 'dist', '!dist/.git']);
-});
-
-gulp.task('serve', serve('dist'));
-
-gulp.task('build', ['sass', 'html', 'js']);
